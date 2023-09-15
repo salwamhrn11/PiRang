cbc205f8238da1bca95208b9c2195510dab805fc
dundorma <90293691+dundorma@users.noreply.github.com>
2023-09-15 14:18:05 +0700

    Init WPF app

---
+++ PiRang-WPF/Class/PerangkatDesa.cs
@@ -0,0 +1,26 @@
+namespace PiRang_WPF
+{
+	class PerangkatDesa
+	{
+		string _username;
+		string _password;
+
+		public string Username
+		{
+			get { return _username; }
+			set { _username = value; }
+		}
+		public string password { get { return _password; } set { _password = value; } }
+
+
+		public void Save()
+		{ // to be implemented
+		  //
+		}
+
+		public void Edit()
+		{ // to be implemented
+		  //
+		}
+	}
+}
