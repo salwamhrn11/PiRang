ffb71558240f065568c209bb0fb080f4fbc6cd18
dundorma <90293691+dundorma@users.noreply.github.com>
2023-11-16 04:09:44 +0700

    feat: add user's crud

--- PiRang-WPF/PiRang-WPF/DBComm/WargaWrapper.cs
+++ PiRang-WPF/PiRang-WPF/DBComm/WargaWrapper.cs
@@ -32,4 +32,54 @@ public partial class NpgsqlWrapper
             return wargaList;
         }
     }
+
+    public bool AddWarga(string email, string no_hp, string password)
+    {
+        try
+        {
+            string sql = @"select * from warga_insert(:_email,:_no_hp,:_password)";
+            NpgsqlCommand cmd = new NpgsqlCommand(sql, connection);
+            cmd.Parameters.AddWithValue("_email", email);
+            cmd.Parameters.AddWithValue("_no_hp", no_hp);
+            cmd.Parameters.AddWithValue("_password", password);
+
+            if ((int)cmd.ExecuteScalar() == 0)
+            {
+                MessageBox.Show("Terjadi error dalam add warga", "Warning", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return false;
+            }
+
+            return true;
+        }
+        catch (Exception ex)
+        {
+            MessageBox.Show("Error: ." + ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            return false;
+        }
+    }
+
+    public bool EditWarga(string email, string no_hp, string password)
+    {
+        try
+        {
+            string sql = @"select * from warga_update(:_email,:_no_hp,:_password)";
+            NpgsqlCommand cmd = new NpgsqlCommand(sql, connection);
+            cmd.Parameters.AddWithValue("_email", email);
+            cmd.Parameters.AddWithValue("_no_hp", no_hp);
+            cmd.Parameters.AddWithValue("_password", password);
+
+            if ((int)cmd.ExecuteScalar() == 0)
+            {
+                MessageBox.Show("Terjadi error dalam edit warga", "Warning", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return false;
+            }
+
+            return true;
+        }
+        catch (Exception ex)
+        {
+            MessageBox.Show("Error: ." + ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            return false;
+        }
+    }
 }
--- PiRang-WPF/PiRang-WPF/View/AdminListUser.xaml.cs
+++ PiRang-WPF/PiRang-WPF/View/AdminListUser.xaml.cs
@@ -16,29 +16,76 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace PiRang_WPF.View
+namespace PiRang_WPF.View;
+
+/// <summary>
+/// Interaction logic for AdminListUser.xaml
+/// </summary>
+public partial class AdminListUser : UserControl
 {
-    /// <summary>
-    /// Interaction logic for AdminListUser.xaml
-    /// </summary>
-    public partial class AdminListUser : UserControl
+    public AdminListUser()
     {
-        public AdminListUser()
+        InitializeComponent();
+        NpgsqlWrapper wrapper = new NpgsqlWrapper();
+        wrapper.load();
+        wrapper.connect();
+        dgUsers.ItemsSource = wrapper.GetAllWarga();
+        wrapper.disconnect();
+    }
+
+    private void btnAddClick(object sender, RoutedEventArgs e)
+    {
+        NpgsqlWrapper npgsqlWrapper = new NpgsqlWrapper();
+        npgsqlWrapper.load();
+        npgsqlWrapper.connect();
+
+        if (!npgsqlWrapper.AddWarga(txtEmail.Text, txtNoHp.Text, txtPassword.Text))
         {
-            InitializeComponent();
-            NpgsqlWrapper wrapper = new NpgsqlWrapper();
-            wrapper.load();
-            wrapper.connect();
-            dgUsers.ItemsSource = wrapper.GetAllWarga();
-            wrapper.disconnect();
+            MessageBox.Show("Gagal menambah warga", "Failed", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+        else
+        {
+            MessageBox.Show("Berhasil menambah warga", "Success", MessageBoxButton.OK, MessageBoxImage.Information);
+            dgUsers.ItemsSource = npgsqlWrapper.GetAllWarga();
         }
 
-        private void btnAddClick(object sender, RoutedEventArgs e)
-        {
-        }
+        npgsqlWrapper.disconnect();
+    }
 
-        private void btnEditClick(object sender, RoutedEventArgs e)
+    private void btnEditClick(object sender, RoutedEventArgs e)
+    {
+        NpgsqlWrapper wrapper = new NpgsqlWrapper();
+        wrapper.load();
+        wrapper.connect();
+
+        if (!wrapper.EditWarga(txtEmail.Text, txtNoHp.Text, txtPassword.Text))
         {
+            MessageBox.Show("Gagal edit warga", "Failed", MessageBoxButton.OK, MessageBoxImage.Information);
         }
+        else
+        {
+            MessageBox.Show("Berhasil edit warga", "Success", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+        dgUsers.ItemsSource = wrapper.GetAllWarga();
+
+        wrapper.disconnect();
+    }
+    private void TextBox_NumberOnly(object sender, TextCompositionEventArgs e)
+    {
+        if (!IsNumber(e.Text))
+        {
+            e.Handled = true;
+        }
+    }
+
+    private bool IsNumber(string text)
+    {
+        // Use regular expression to check if the text is a valid integer.
+        return System.Text.RegularExpressions.Regex.IsMatch(text, "^[0-9]*$");
+    }
+
+    private void dgUsers_SelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+        MessageBox.Show("Selected Item: " + dgUsers.SelectedItem.ToString());
     }
 }
