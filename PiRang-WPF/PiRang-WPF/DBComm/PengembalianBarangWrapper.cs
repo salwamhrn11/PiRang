38e862fe84eb83967bb96bfe492f84202a012e05
dundorma <90293691+dundorma@users.noreply.github.com>
2023-11-16 06:02:30 +0700

    feat: add pengembalian_barang's crud

--- PiRang-WPF/PiRang-WPF/DBComm/PengembalianBarangWrapper.cs
+++ PiRang-WPF/PiRang-WPF/DBComm/PengembalianBarangWrapper.cs
@@ -32,4 +32,30 @@ public partial class NpgsqlWrapper
             return pengembalianBarangs;
         }
     }
+
+    public bool AddPengembalianBarang(int barang_id, string email, int jumlah_kembali, string kondisi_barang, int jumlah_dipinjam)
+    {
+        try
+        {
+            string sql = @"select * from pengembalian_insert(:_barang_id,:_email,:_jumlah_kembali,:_kondisi_barang,:_jumlah_dipinjam)";
+            NpgsqlCommand cmd = new NpgsqlCommand(sql, connection);
+            cmd.Parameters.AddWithValue("_barang_id", barang_id);
+            cmd.Parameters.AddWithValue("_email", email);
+            cmd.Parameters.AddWithValue("_jumlah_kembali", jumlah_kembali);
+            cmd.Parameters.AddWithValue("_kondisi_barang", kondisi_barang);
+            cmd.Parameters.AddWithValue("_jumlah_dipinjam", jumlah_dipinjam);
+
+            if ((int)cmd.ExecuteScalar() == 0)
+            {
+                MessageBox.Show("Terjadi error dalam add barang", "Warning", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return false;
+            }
+            return true;
+        }
+        catch (Exception ex)
+        {
+            MessageBox.Show("error: " + ex.Message, "Error Occured", MessageBoxButton.OK, MessageBoxImage.Error);
+            return false;
+        }
+    }
 }
